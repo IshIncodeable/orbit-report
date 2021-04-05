0e3518c836543895ef2177d276b4ca9985096720
Ishmael Ben Israel <Ishincodeable@gmail.com>
2021-04-05 14:42:27 -0500

    Css styling w/ attribute Directives 4 Space Debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,8 +1,8 @@
 export class Satellite {
     name: string;
-    orbitType: string;
     type: string;
     operational: boolean;
+    orbitType: string;
     launchDate: string;
 
     constructor(name: string, type: string, operational: boolean,  orbitType: string, launchDate: string ) {
@@ -12,6 +12,15 @@ export class Satellite {
         this.orbitType = orbitType;
         this.launchDate = launchDate;
 
+    }
+
+    shouldShowWarning(){
+        //Part 6: Highlight Space Debris
+        /*if ( this.type.toLowerCase() === "space debris") {
+
+        }*/
+           
+        return (this.type.toLowerCase() === "space debris")
     }
     
 }
